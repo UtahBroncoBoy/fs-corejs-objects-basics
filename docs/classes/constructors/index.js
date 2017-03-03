86f4b0897bc42c199c5fd46cec003a73d7c644f4
Jeff Fredericks <jeffwfredericks@gmail.com>
2017-03-02 17:48:59 -0700

    Finished constructors assignment.

--- docs/classes/constructors/index.js
+++ docs/classes/constructors/index.js
@@ -1,1 +1,34 @@
-// Code goes here...
+export class Vehicle  {
+  constructor(engineType, numberOfDoors, make) {
+    this._engineType = engineType;
+    this._numberOfDoors = numberOfDoors;
+    this._make = make;
+  }
+
+  get engineType()  {
+    return this._engineType;
+  }
+
+  get numberOfDoors() {
+    return this._numberOfDoors;
+  }
+
+  get make()  {
+    return this._make;
+  }
+}
+
+export class Truck extends Vehicle {
+  constructor(make) {
+    super('V8', 2, make);
+  }
+}
+
+export class Car extends Vehicle {
+  constructor(make) {
+    super('V4', 4, make);
+  }
+}
+
+export const F150 = new Truck('Ford');
+export const Prius = new Car('Toyota');
